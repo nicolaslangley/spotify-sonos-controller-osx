0b9eadc750504863de499c0d959762aace91a955
Nicolas Langley <nicolasqlangley@gmail.com>
2014-10-23 01:55:51 -0700

    Initial Commit

---
+++ SpotifySonosController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpotifySonosController
+//
+//  Created by Nicolas Langley on 10/23/14.
+//  Copyright (c) 2014 Nicolas Langley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
